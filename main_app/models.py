64ae01cdc0aa58b38d6c19dcedd5f592cc4716f9
tjphillips08 <tjphillips08@icloud.com>
2022-10-10 09:15:50 -0500

    created course model for db

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length = 100)
+    img = models.CharField(max_length = 250)
+    address = models.CharField(max_length = 150)
+    website = models.CharField(max_length = 250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
