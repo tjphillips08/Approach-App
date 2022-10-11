71ff1eb819bea3353b8c9951490ae3fc27e8af46
tjphillips08 <tjphillips08@icloud.com>
2022-10-11 12:44:51 -0500

    created a post and comment model

--- main_app/models.py
+++ main_app/models.py
@@ -38,3 +38,36 @@ class Club(models.Model):
 
 
 
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length = 200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+
+        return reverse("post_detail", kwargs={"slug": str(self.slug)})
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    active = models.BooleanField(default = False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return "Comment {} by {}".format(self.body,self.name)
