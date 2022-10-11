356668c6478cc5e93c7e3daf81a3b8c62720c567
tjphillips08 <tjphillips08@icloud.com>
2022-10-11 13:27:34 -0500

    added views and templates to posts and comments

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
-from .models import Course, Member, Club
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Course, Member, Club, Post, Comment
 
 # Register your models here.
 
 admin.site.register(Course)
 admin.site.register(Member)
 admin.site.register(Club)
+
+
+
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ("title", "slug", "created_on")
+    list_filter = ("created_on",)
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "body", "post", "created_on", "active")
+    list_filter = ("active", "created_on")
+    search_fields = ("name", "email", "body")
+    actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+admin.site.register(Post, PostAdmin)
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View 
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-from .models import Course, Member, Club
+from .models import Course, Member, Club, Post
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.views import generic
+from .forms import CommentForm
+
 
 
 # Create your views here.
@@ -118,6 +121,33 @@ class ClubCreate(CreateView):
     def get_success_url(self):
         return reverse('club_detail', kwargs={'pk': self.object.pk})
 
+
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.order_by('-created_on')
+    template_name = "post_list.html"
+    paginate_by = 3
+
+
+
+def post_detail(request, slug):
+    template_name = "post_detail.html"
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True).order_by("-created_on")
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {"post": post, "comments": comments, "new_comment": new_comment, "comment_form": comment_form})
 
 
 
